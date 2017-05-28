f48a6dbbb9888a208805fcef79a1533d0f69da99
vacaran2017 <vacaran2017@yandex.com>
2017-05-28 02:29:23 -0700

    cool js shit

---
+++ js.js
@@ -0,0 +1,62 @@
+let other = {
+	ja: 'en',
+	en: 'ja',
+}
+
+function createToc() {
+	let toc = {
+		ja: ['<nav><h2>目録</h2><ul>', '</ul></nav>'],
+		en: ['<nav><h2>Contents</h2><ul>', '</ul></nav>'],
+	}
+
+	// for each <section>
+	$('section').each(function(index) {
+		let lang = $(this).attr('lang');
+
+		// build the toc <li> elements from each header
+		$(this).find('h1, h2, h3, h4, h5, h6').each(function(index) {
+			let tagName = $(this).prop('tagName');
+			let headerLevel = tagName.substr(1,1) // save the header level for the CSS class later
+			let id = 'header-' + index + '-' + lang;
+			let headerText = $(this).text();
+
+			$(this).attr('id', id);
+
+			console.log(headerLevel, headerText);
+
+			// I considered making nested HTML lists which would be more "proper" but I don't feel like it, have nesting emulated using CSS indents
+			let element = '<li><a class="toc__level--' + headerLevel + '" href="#' + id + '">' + headerText + '</a></li>';
+			toc[lang].splice(1 + index, 0, element);
+		});
+
+		// convert the toc array to a string and insert it into the beginning of the <section>
+		$(this).prepend(toc[lang].join(''));
+
+		// add numbers to each <p>
+		$(this).find('p').each(function(index) {
+			let number = index + 1;
+
+			let id = lang + '-' + number;
+			let href = '#' + other[lang] + '-' + number; // make the number a link to the corresponding paragraph in the other column
+			$(this).prepend('<a class="p__number" id="' + id + '" href="' + href + '">' + number + '</a>');
+		});
+	});
+}
+
+function paragraphHighlight() {
+	// on mouse over a paragraph
+	$('p').mouseover(function() {
+		$('.p--target').removeClass('p--target'); // unhighlight any highlighted paragraphs
+
+		let thisId = '#' + $(this).children('.p__number').attr('id');
+		$(thisId).parent().addClass('p--target'); // add highlight to the parent <p> of the number
+
+		let otherId = thisId.replace(/(en|ja)/, function(match, lang, offset, string) { return other[lang]; });
+		$(otherId).parent().addClass('p--target'); // add highlight to the corresponding <p> in the other column too
+	});
+}
+
+$(document).ready(function() {
+	createToc();
+	paragraphHighlight();
+});
